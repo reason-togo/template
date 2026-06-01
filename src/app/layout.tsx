bb0da8ff63516713f26672d6c6fc2a6507f55c69
rebi13 <rebi13@naver.com>
2026-06-02 00:12:56 +0900

    Chore: 폰트 weight 최적화 및 의존성 업데이트

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Noto_Sans_KR } from "next/font/google";
 import "./globals.css";
 
 const notoSansKR = Noto_Sans_KR({
-  weight: ["300", "400", "500", "600", "700", "800"],
+  weight: ["400", "600", "700"],
   subsets: ["latin"],
   display: "swap",
 });
